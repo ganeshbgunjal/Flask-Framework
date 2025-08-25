be272a0812f63660d0f16ab6e06cb5bdf9dd4e8c
ganeshgunjal118@gmail.com <ganeshgunjal118@gmail.com>
2025-08-25 09:37:05 +0530

    file update

--- flask/getpost.py
+++ flask/getpost.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 '''
 It creates an instance of the flask app. 
@@ -13,13 +13,20 @@ def welcome():
     return '''<html><H1>Welcome to this Data Scientist course!</H1></html>\
             <html><H2>Become a Pro Data Scientist!</H2></html>'''
 
-@app.route("/index")
+@app.route("/index",methods=['GET'])
 def index():
     return render_template('index.html')
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/form',methods=['GET','POST'])
+def form():
+    if request.method=='POST':
+        name = request.form['name']
+        return f'Hello {name}!'
+    return render_template('form.html')
 
 
 if __name__ == '__main__':     # This is the entry point of any .py file.
