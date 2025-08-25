b3582d4b289f8e5affa59de0049137467e3d2f75
ganeshgunjal118@gmail.com <ganeshgunjal118@gmail.com>
2025-08-25 08:04:24 +0530

    file add

---
+++ flask/getpost.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+'''
+It creates an instance of the flask app. 
+which will be your WSGI (web server gateway interface) application.
+'''
+
+#### WSGI application:-
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return '''<html><H1>Welcome to this Data Scientist course!</H1></html>\
+            <html><H2>Become a Pro Data Scientist!</H2></html>'''
+
+@app.route("/index")
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+if __name__ == '__main__':     # This is the entry point of any .py file.
+    app.run(debug=True)                   # to run app
+
