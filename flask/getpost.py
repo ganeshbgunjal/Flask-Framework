21a84bdc836e12a053ade5fa4b283653ab84fe23
ganeshgunjal118@gmail.com <ganeshgunjal118@gmail.com>
2025-08-26 23:53:33 +0530

    file update

--- flask/getpost.py
+++ flask/getpost.py
@@ -28,12 +28,12 @@ def form():
         return f'Hello {name}!'
     return render_template('form.html')
 
-# @app.route('/submit', methods=['GET','POST'])
-# def submit():
-#     if request.method == 'POST':
-#         name = request.form['name']
-#         return f'Hello {name}!'
-#     return render_template('form.html')
+@app.route('/submit', methods=['GET','POST'])
+def submit():
+    if request.method == 'POST':
+        name = request.form['name']
+        return f'Hello {name}!'
+    return render_template('form.html')
 
 
 
